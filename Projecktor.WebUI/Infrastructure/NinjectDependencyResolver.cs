387aa566e5075c01bba4a89b0258b91f5f212179
DarrenDDaley <darren.daley@hotmail.co.uk>
2016-07-03 20:01:03 +0100

    Added Ninject to the project

---
+++ Projecktor.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace Projecktor.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel) {
+            this.kernel = kernel;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType) {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType) {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings() {
+            // put bindings here
+        }
+    }
+}
