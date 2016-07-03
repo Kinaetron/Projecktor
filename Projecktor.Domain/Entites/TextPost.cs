10457d8e87807d4fba234e0ea3d84d2ce759b6f8
DarrenDDaley <darren.daley@hotmail.co.uk>
2016-07-03 20:46:56 +0100

    Added a couple of data entities

---
+++ Projecktor.Domain/Entites/TextPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projecktor.Domain.Entites
+{
+    public class TextPost
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public virtual User Author { get; set; }
+
+        public string Text { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ Projecktor.Domain/Entites/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Projecktor.Domain.Entites
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime DateCreated { get; set; }
+
+
+        private ICollection<TextPost> textPosts;
+        public virtual ICollection<TextPost> TextPosts
+        {
+            get { return textPosts ?? (textPosts = new Collection<TextPost>()); }
+            set { textPosts = value; }
+        }
+
+        private ICollection<User> following;
+        public virtual ICollection<User> Following
+        {
+            get { return following ?? (following = new Collection<User>()); }
+            set { following = value; }
+        }
+
+        private ICollection<User> followers;
+        public virtual ICollection<User> Followers
+        {
+            get { return following ?? (followers = new Collection<User>()); }
+            set { followers = value; }
+        }
+    }
+}
