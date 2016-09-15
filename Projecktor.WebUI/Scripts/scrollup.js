7a1788d5e37cafc294ca6ed1e3b81c168d8df7cb
DarrenDDaley <darren.daley@hotmail.co.uk>
2016-09-15 20:14:41 +0100

    Added in a srcoll up button

---
+++ Projecktor.WebUI/Scripts/scrollup.js
@@ -0,0 +1,13 @@
+﻿$(window).scroll(function () {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$(document).ready(function () {
+    $("#toTop").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    });
+});
