145ec370ac3e04a90486af5de2f8251096e5fe07
DarrenDDaley <darren.daley@hotmail.co.uk>
2016-09-17 08:00:48 +0100

    readded textpostScript

---
+++ Projecktor.WebUI/Scripts/textpostScript.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    $('#textpost').on('click', function () {
+        if ($(this).hasClass('picked')) {
+            unselect($(this));
+        }
+        else {
+            $(this).addClass('picked');
+            $('.open').slideFadeToggle();
+        }
+        return false;
+    });
+
+    $('.stop').on('click', function () {
+        unselect($('#textpost'));
+        return false;
+    });
+});
+
+function unselect(e) {
+    $('.open').slideFadeToggle(function () {
+        e.removeClass('picked');
+    });
+}
+
+$.fn.slideFadeToggle = function (easing, callback) {
+    return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+};
