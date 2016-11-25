76b10810295cc65012ebaf7b8d5fa57055cd2590
Darren Daley <darren.daley@hotmail.co.uk>
2016-11-25 17:14:00 +0000

    fixed issue with rendering scripts

--- Projecktor.WebUI/App_Start/BundleConfig.cs
+++ Projecktor.WebUI/App_Start/BundleConfig.cs
@@ -26,20 +26,20 @@ namespace Projecktor.WebUI
                 "~/Scripts/modernizr-2.6.2.js",
                 "~/Scripts/jquery-ui-1.12.1.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/customscripts").Include(
-                 "~/Scripts/actionScripts.js",
-                 "~/Scripts/imageGalleryScript.js",
-                 "~/Scripts/imagepostScript.js",
-                 "~/Scripts/infiniteScrollingDashboard.js",
-                 "~/Scripts/infiniteScrollingScript.js",
-                 "~/Scripts/infiniteScrollingUserLikes.js",
-                 "~/Scripts/infinteScrollingUserpage.js",
-                 "~/Scripts/infintiteScrollingDashboardLikes.js",
-                 "~/Scripts/postValidation.js",
-                 "~/Scripts/scrollup.js",
-                 "~/Scripts/searchScript.js",
-                 "~/Scripts/textpostScript.js"
-                 ));
+            //bundles.Add(new ScriptBundle("~/bundles/customscripts").Include(
+            //     "~/Scripts/actionScripts.js",
+            //     "~/Scripts/imageGalleryScript.js",
+            //     "~/Scripts/imagepostScript.js",
+            //     "~/Scripts/infiniteScrollingDashboard.js",
+            //     "~/Scripts/infiniteScrollingScript.js",
+            //     "~/Scripts/infiniteScrollingUserLikes.js",
+            //     "~/Scripts/infinteScrollingUserpage.js",
+            //     "~/Scripts/infintiteScrollingDashboardLikes.js",
+            //     "~/Scripts/postValidation.js",
+            //     "~/Scripts/scrollup.js",
+            //     "~/Scripts/searchScript.js",
+            //     "~/Scripts/textpostScript.js"
+            //     ));
         }
     }
 }
