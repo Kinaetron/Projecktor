d5f28f6e599fb74441e1c3bf736a45b8a350b5f5
DarrenDDaley <darren.daley@hotmail.co.uk>
2016-12-11 20:41:54 +0000

    Further changed improved the upload forms

---
+++ Projecktor.WebUI/Scripts/formGlass.js
@@ -0,0 +1,9 @@
+﻿function GlassOn() {
+    $(".post-forms-glass").css({ "opacity": "1", "display": "block" });
+    $("body").css({ "overflow": "hidden" });
+}
+
+function GlassOff() {
+    $(".post-forms-glass").css({ "opacity": "0", "display": "none" });
+    $("body").css({ "overflow": "hidden", "height": "100%" });
+}
